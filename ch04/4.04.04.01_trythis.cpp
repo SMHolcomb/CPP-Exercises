5a62b847ad861c48025adca02f62fdf440fd6485
Stephanie <stephanie.holcomb@gmail.com>
2017-10-14 14:24:14 -0700

    added for-statement

--- ch04/4.04.04.01_trythis.cpp
+++ ch04/4.04.04.01_trythis.cpp
@@ -2,12 +2,19 @@
 
 int main() {
 
+	std::cout << "**** Using a while statement loop  ****" << std::endl << std::endl;
 	int c = 'a';
 	std::cout << c << std::endl;
 
 	while (c <= 'z') {
 		std::cout << char(c) << '\t' << c << std::endl;
 		++c;
+	}
 
+	std::cout << std::endl << "****  Using a for-statement loop  ****" << std::endl<< std::endl;
+
+	// using for loop
+	for (int i = 'a'; i <='z'; i++) {
+		std::cout << char(i) << '\t' << i << std::endl;
 	}
 }
